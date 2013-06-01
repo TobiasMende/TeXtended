598bda1a509e2e747983bf9d63cea869ed92c2dc
Tobias Hecht <Tobias@Tobiass-MacBook-Pro.local>
2013-06-01 16:51:26 +0200

    Added a FileViewTitle class to handle mouse events.

---
+++ TeXtended/TeXtended/FileViewer/FileViewTitle.m
@@ -0,0 +1,34 @@
+//
+//  FileViewTitle.m
+//  TeXtended
+//
+//  Created by Tobias Hecht on 01.06.13.
+//  Copyright (c) 2013 Tobias Mende. All rights reserved.
+//
+
+#import "FileViewTitle.h"
+
+@implementation FileViewTitle
+
+- (id)initWithFrame:(NSRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+    // Drawing code here.
+}
+
+- (void)rightMouseDown:(NSEvent *)theEvent
+{
+    NSLog(@"Box");
+    return;
+}
+
+@end
