fb57a37bcd71ce83256fe643e8c5a3ab99c19908
bannach <bannach@informatik.uni-luebeck.de>
2013-06-09 14:21:21 +0200

    Added FileCompiler.

---
+++ TeXtended/TeXtended/Compiler/FileCompiler.h
@@ -0,0 +1,13 @@
+//
+//  FileCompiler.h
+//  TeXtended
+//
+//  Created by Max Bannach on 09.06.13.
+//  Copyright (c) 2013 Tobias Mende. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FileCompiler : NSObject
+
+@end
