4703595ed0f410342ea72b4b739850dea6dee20f
Tobias Mende <tobias.mende@tobsolution.de>
2013-09-28 13:51:35 +0200

    Final Log Color

--- TeXtended/TeXtended/Utils/Logger/TMTLog.m
+++ TeXtended/TeXtended/Utils/Logger/TMTLog.m
@@ -18,7 +18,7 @@
     [DDLog addLogger:[DDTTYLogger sharedInstance]];
     [[DDTTYLogger sharedInstance] setLogFormatter:[[TMTLogFormatter alloc] initExtended:NO]];
     [[DDASLLogger sharedInstance] setLogFormatter:[[TMTLogFormatter alloc] initExtended:YES]];
-    [[DDTTYLogger sharedInstance] setForegroundColor:[NSColor colorWithCalibratedRed:0.30f green:0.75f blue:0.34f alpha:1.00f] backgroundColor:Nil forFlag:LOG_FLAG_INFO];
+    //[[DDTTYLogger sharedInstance] setForegroundColor:[NSColor colorWithCalibratedRed:0.30f green:0.75f blue:0.34f alpha:1.00f] backgroundColor:Nil forFlag:LOG_FLAG_INFO];
     [[DDTTYLogger sharedInstance] setForegroundColor:[NSColor colorWithCalibratedRed:0.31f green:0.51f blue:0.53f alpha:1.00f] backgroundColor:nil forFlag:LOG_FLAG_VERBOSE];
 }
 
