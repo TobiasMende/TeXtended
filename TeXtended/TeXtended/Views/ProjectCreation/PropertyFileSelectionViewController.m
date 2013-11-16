ee29eee4e60430a928e1b9c380b73cb503be654c
bannach <bannach@informatik.uni-luebeck.de>
2013-11-16 15:05:07 +0100

    Added properties selection (refs #234)

--- TeXtended/TeXtended/Views/ProjectCreation/PropertyFileSelectionViewController.m
+++ TeXtended/TeXtended/Views/ProjectCreation/PropertyFileSelectionViewController.m
@@ -20,6 +20,20 @@
 }
 
 - (IBAction)select:(id)sender {
+    NSOpenPanel *panel = [[NSOpenPanel alloc] init];
+    
+    panel.canChooseFiles = YES;
+    panel.canChooseDirectories = NO;
+    panel.title = NSLocalizedString(@"Choose Properties File", @"Choose Properties File");
+    panel.canCreateDirectories = NO;
+    
+    [panel beginWithCompletionHandler:^(NSInteger result) {
+        if (result == NSFileHandlingPanelOKButton) {
+            NSURL *directory = [panel URL];
+            self.filePath = [directory path];
+        }
+    }];
+    
 }
 
 @end
