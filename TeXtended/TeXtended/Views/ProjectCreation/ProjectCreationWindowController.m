cc8f1cb0fd7f696fc54d209dffafce15a96ef80d
Tobias Mende <tobias.mende@tobsolution.de>
2013-11-16 14:57:04 +0100

    Default collapsed states (refs #234)

--- TeXtended/TeXtended/Views/ProjectCreation/ProjectCreationWindowController.m
+++ TeXtended/TeXtended/Views/ProjectCreation/ProjectCreationWindowController.m
@@ -12,6 +12,7 @@
 #import "MainDocumentSelectionViewController.h"
 #import "PropertyFileSelectionViewController.h"
 #import "DMPaletteContainer.h"
+#import "DMPaletteSectionView.h"
 
 @interface ProjectCreationWindowController ()
 
@@ -34,12 +35,19 @@
     compilerSettings = [CompilerSettingsViewController new];
     propertySelection = [PropertyFileSelectionViewController new];
     mainOcumentSelection = [[MainDocumentSelectionViewController alloc] init];
+    
+    DMPaletteSectionView *projectPathContainer =   [[DMPaletteSectionView alloc] initWithContentView:folderSelection.view andTitle:@"Project Path"];
+    DMPaletteSectionView *mainAndBibcontainer = [[DMPaletteSectionView alloc] initWithContentView:mainOcumentSelection.view andTitle:@"Maindocuments and Bibfiles"];
+    DMPaletteSectionView *propertyFileContainer = [[DMPaletteSectionView alloc] initWithContentView:propertySelection.view andTitle:@"Select a property file"];
+    DMPaletteSectionView *compilerSettingContainer = [[DMPaletteSectionView alloc] initWithContentView:compilerSettings.view andTitle:@"Compiler Settings"];
     container.sectionViews = [NSArray arrayWithObjects:
-                            [[DMPaletteSectionView alloc] initWithContentView:folderSelection.view andTitle:@"Project Path"],
-                            [[DMPaletteSectionView alloc] initWithContentView:mainOcumentSelection.view andTitle:@"Maindocuments and Bibfiles"],
-                              [[DMPaletteSectionView alloc] initWithContentView:propertySelection.view andTitle:@"Select a property file"],
-                            [[DMPaletteSectionView alloc] initWithContentView:compilerSettings.view andTitle:@"Compiler Settings"],
+                          projectPathContainer,
+                            mainAndBibcontainer,
+                              propertyFileContainer,
+                            compilerSettingContainer,
                               nil];
+    [container collapseSectionView:propertyFileContainer];
+    [container collapseSectionView:compilerSettingContainer];
     container.sectionHeaderGradientEndColor = [NSColor colorWithCalibratedRed:0.95f green:0.95f blue:0.95f alpha:1.00f];
     container.sectionHeaderGradientStartColor = [NSColor colorWithCalibratedRed:0.82f green:0.82f blue:0.82f alpha:1.00f];
 }
--- TeXtended/TeXtended/Views/ProjectCreation/PropertyFileSelectionViewController.h
+++ TeXtended/TeXtended/Views/ProjectCreation/PropertyFileSelectionViewController.h
@@ -10,4 +10,5 @@
 
 @interface PropertyFileSelectionViewController : NSViewController
 
+
 @end
