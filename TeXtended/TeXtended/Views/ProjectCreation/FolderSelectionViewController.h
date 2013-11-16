b95a2c25d7f699a20cc43ccc21758406ab1b0fa2
bannach <bannach@informatik.uni-luebeck.de>
2013-11-16 13:37:35 +0100

    Added DMInspectorPalett (refs #234)

---
+++ TeXtended/TeXtended/Views/ProjectCreation/FolderSelectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  FolderSelectionViewController.h
+//  TeXtended
+//
+//  Created by Max Bannach on 16.11.13.
+//  Copyright (c) 2013 Tobias Mende. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface FolderSelectionViewController : NSViewController
+
+@end
---
+++ TeXtended/TeXtended/Views/ProjectCreation/FolderSelectionViewController.m
@@ -0,0 +1,26 @@
+//
+//  FolderSelectionViewController.m
+//  TeXtended
+//
+//  Created by Max Bannach on 16.11.13.
+//  Copyright (c) 2013 Tobias Mende. All rights reserved.
+//
+
+#import "FolderSelectionViewController.h"
+
+@interface FolderSelectionViewController ()
+
+@end
+
+@implementation FolderSelectionViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Initialization code here.
+    }
+    return self;
+}
+
+@end
