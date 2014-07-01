945cabf7c20378955e30a40b489e6c5b07f9d94d
Tobias Mende <tobias.mende@tobsolution.de>
2014-07-01 20:55:08 +0200

    Adding a StartWindow (just UI, no function atm) [refs #651]

---
+++ TeXtended/TeXtended/Views/StartScreen/StartScreenWindowController.h
@@ -0,0 +1,13 @@
+//
+//  StartScreenWindowController.h
+//  TeXtended
+//
+//  Created by Tobias Mende on 01.07.14.
+//  Copyright (c) 2014 Tobias Mende. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface StartScreenWindowController : NSWindowController
+
+@end
---
+++ TeXtended/TeXtended/Views/StartScreen/StartScreenWindowController.m
@@ -0,0 +1,33 @@
+//
+//  StartScreenWindowController.m
+//  TeXtended
+//
+//  Created by Tobias Mende on 01.07.14.
+//  Copyright (c) 2014 Tobias Mende. All rights reserved.
+//
+
+#import "StartScreenWindowController.h"
+
+@interface StartScreenWindowController ()
+
+@end
+
+@implementation StartScreenWindowController
+
+- (id)initWithWindow:(NSWindow *)window
+{
+    self = [super initWithWindow:window];
+    if (self) {
+        // Initialization code here.
+    }
+    return self;
+}
+
+- (void)windowDidLoad
+{
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
