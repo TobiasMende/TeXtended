1bd7a331e3bcbd1c5f0343ba510ac2e811adc444
Tobias Mende <tobias.mende@tobsolution.de>
2014-07-02 13:18:21 +0200

    adding NSInvocationOperation to queue retains the table view [fixes #671]

---
+++ TeXtended/TeXtended/Editor/LightHighlightingTextView.h
@@ -0,0 +1,13 @@
+//
+//  LightHighlightingTextView.h
+//  TeXtended
+//
+//  Created by Tobias Mende on 02.07.14.
+//  Copyright (c) 2014 Tobias Mende. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface LightHighlightingTextView : NSText
+
+@end
---
+++ TeXtended/TeXtended/Editor/LightHighlightingTextView.m
@@ -0,0 +1,29 @@
+//
+//  LightHighlightingTextView.m
+//  TeXtended
+//
+//  Created by Tobias Mende on 02.07.14.
+//  Copyright (c) 2014 Tobias Mende. All rights reserved.
+//
+
+#import "LightHighlightingTextView.h"
+
+@implementation LightHighlightingTextView
+
+- (id)initWithFrame:(NSRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+    [super drawRect:dirtyRect];
+    
+    // Drawing code here.
+}
+
+@end
