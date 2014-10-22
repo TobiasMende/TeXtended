f72821eeeeef716bc496e87f44f15077ebfe624e
Tobias Mende <tobias.mende@tobsolution.de>
2014-10-22 15:05:23 +0200

    fixing file view scroll issue [fixes #693]

--- TeXtended/TeXtended/Views/FileView/FileOutlineView.m
+++ TeXtended/TeXtended/Views/FileView/FileOutlineView.m
@@ -84,19 +84,7 @@
 
     - (void)setViewController:(NSViewController *)newController
     {
-        if (viewController) {
-            NSResponder *controllerNextResponder = [viewController nextResponder];
-            [super setNextResponder:controllerNextResponder];
-            [viewController setNextResponder:nil];
-        }
-
         viewController = newController;
-
-        if (newController) {
-            NSResponder *ownNextResponder = [self nextResponder];
-            [super setNextResponder:viewController];
-            [viewController setNextResponder:ownNextResponder];
-        }
     }
 
     - (void)setNextResponder:(NSResponder *)newNextResponder
